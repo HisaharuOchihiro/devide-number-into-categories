811925d7df3df465d2afd345dd9b69fee0fba996
Максим <@DESKTOP-9JOEI7F>
2023-03-19 02:45:40 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <vector>
+#include <array>
+
+std::vector<long long> numbers;
+void ebanina()
+{
+	long long chislo;
+	std::cin >> chislo;
+	long long del_ostacha = 10, del_delenie = 1;
+	int i = 0;
+	long long ostacha, delenie;
+
+	int a = 1;
+	while (i == 0)
+	{
+		ostacha = chislo % del_ostacha;
+		delenie = ostacha / del_delenie;
+		numbers.push_back(delenie);
+		a++;
+		del_ostacha *= 10;
+		del_delenie *= 10;
+		if (ostacha / chislo == 1)
+		{
+			break;
+		}
+
+	}
+	int u = 0;
+	for (long long i = numbers.size() - 1; i >= 0; i--)
+	{
+
+		u++;
+		std::cout << "Chislo#" << u << " " << numbers[i] << std::endl;
+	}
+
+}
+
+int main() 
+{
+	ebanina();
+	int a;
+	std::cin >>  a;
+	
+}
